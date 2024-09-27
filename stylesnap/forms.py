0ed7e648b03f35f5d2c79fa6dd8c3856a8d22d48
potdarsrushti4 <potdarsrushti4@gmail.com>
2024-09-27 22:54:31 +0530

    Added CSS to register.html and also updated the forms.py for the same

--- stylesnap/forms.py
+++ stylesnap/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
+import re
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -14,8 +15,33 @@ class LoginForm(AuthenticationForm):
     }))
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email'
+    }))
+    username = forms.CharField(max_length=150, 
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Username'
+                               }),
+                               help_text="")  # Blank out the help text
+
+    password1 = forms.CharField(label="New Password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+    }))
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Confirm Password'
+    }))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    # Override the clean_username method to explicitly validate the username conditions
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.match(r'^[\w.@+-]+$', username):
+            raise ValidationError("Username can only contain letters, digits and @/./+/-/_ characters.")
+        return username
