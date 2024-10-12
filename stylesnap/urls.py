797282b75a2d3a0af6290c5e4471cca48acdaa17
anu <joshi2017anu@gmail.com>
2024-10-12 15:22:29 +0530

    Revert "base added"
    This reverts commit f3c4e7dc5af48e5e749af2931e473af87c94aa6d.

--- stylesnap/urls.py
+++ stylesnap/urls.py
@@ -24,7 +24,5 @@ urlpatterns = [
     path('login/',views.login_view,name='login'),
     path('register/', views.register_view, name='register'),
     path('chatbot/',views.chatbot,name="chatbot"),
-    path('chatbot2/',views.analyze_image,name="chatbot2"),
-
 
 ]
